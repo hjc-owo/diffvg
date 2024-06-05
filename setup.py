25b202d7d1e7a8256bca294065ee6921d82d3c67
hjc-owo <2535199657@qq.com>
2024-06-05 17:15:51 +0800

    fix(env): setup

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ class Build(build_ext):
             env = os.environ.copy()
             env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
                                                                   self.distribution.get_version())
+            env['PYTHON_INCLUDE_DIRS'] = include_path
             if not os.path.exists(self.build_temp):
                 os.makedirs(self.build_temp)
             subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
